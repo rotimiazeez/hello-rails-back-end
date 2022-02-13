e5ccb02cc227fd9aad36fdda7b04dba6b87f2b76
rotimiazeez <harzeyzhrotimi@gmail.com>
2022-02-12 22:06:03 -0800

    fix: Fix linter errors

--- test/controllers/greetings_controller_test.rb
+++ test/controllers/greetings_controller_test.rb
@@ -1,34 +1,34 @@
-require "test_helper"
+require 'test_helper'
 
 class GreetingsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @greeting = greetings(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get greetings_url, as: :json
     assert_response :success
   end
 
-  test "should create greeting" do
+  test 'should create greeting' do
     assert_difference('Greeting.count') do
-      post greetings_url, params: { greeting: {  } }, as: :json
+      post greetings_url, params: { greeting: {} }, as: :json
     end
 
     assert_response 201
   end
 
-  test "should show greeting" do
+  test 'should show greeting' do
     get greeting_url(@greeting), as: :json
     assert_response :success
   end
 
-  test "should update greeting" do
-    patch greeting_url(@greeting), params: { greeting: {  } }, as: :json
+  test 'should update greeting' do
+    patch greeting_url(@greeting), params: { greeting: {} }, as: :json
     assert_response 200
   end
 
-  test "should destroy greeting" do
+  test 'should destroy greeting' do
     assert_difference('Greeting.count', -1) do
       delete greeting_url(@greeting), as: :json
     end
